326074905f8c0bbb3cb01de4ab475d527829f153
Oleg Chumin <tschumin.oleg@gmail.com>
2022-12-29 11:58:55 +0300

    Add new file

---
+++ src/main/java/Advanced_Java/TestCase.java
@@ -0,0 +1,10 @@
+package Advanced_Java;
+
+public class TestCase {
+    /**
+     * Артефакт - артефакт, описывающий совокупность шагов, условий и параметров необходимых для проверки
+     * реализации тестируемой функции
+     * Action -> Expected Result -> Test Result
+     * Open page "login" -> Login page is opened -> Passed
+     * */
+}
