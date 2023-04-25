f49364d0bad7290e3a74a280cd9ed062f8dd6a1e
Oleg <tschumin.oleg@gmail.com>
2023-04-25 23:11:03 +0300

    Add new files

---
+++ src/main/java/Advanced_Java/Part_04_RegExp/Lesson_032.java
@@ -0,0 +1,22 @@
+package Advanced_Java.Part_04_RegExp;
+
+import java.util.Arrays;
+
+public class Lesson_032 {
+    public static void main(String[] args) {
+        String str = "Hello there hey";
+        String regex = " ";
+        String[] words = str.split(regex);
+        System.out.println(Arrays.toString(words));
+
+        String str2 = "Hello.there.hey";
+        String regex2 = "\\.";
+        String[] words2 = str2.split(regex2);
+        System.out.println(Arrays.toString(words2));
+
+        String str3 = "Hello453415345345there54626463456hey4562646hey";
+        String regex3 = "\\d+";
+        String[] words3 = str3.split(regex3);
+        System.out.println(Arrays.toString(words3));
+    }
+}
