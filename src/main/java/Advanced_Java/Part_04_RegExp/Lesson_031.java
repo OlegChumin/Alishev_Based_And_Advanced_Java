37ac3bd5db287936a624ebd2c1f78408d519acd8
Oleg <tschumin.oleg@gmail.com>
2023-04-25 17:28:31 +0300

    Add new files

---
+++ src/main/java/Advanced_Java/Part_04_RegExp/Lesson_031.java
@@ -0,0 +1,9 @@
+package Advanced_Java.Part_04_RegExp;
+
+public class Lesson_031 {
+    public static void main(String[] args) {
+        String a = "1234";
+        String b = new String("1234");
+        System.out.println(a.matches(b));
+    }
+}
