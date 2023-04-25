ccf8a57bfc8231160beb059cf03596c36be02ff3
Oleg <tschumin.oleg@gmail.com>
2023-04-25 17:36:26 +0300

    Add new files

--- src/main/java/Advanced_Java/Part_04_RegExp/Lesson_031.java
+++ src/main/java/Advanced_Java/Part_04_RegExp/Lesson_031.java
@@ -3,7 +3,18 @@ package Advanced_Java.Part_04_RegExp;
 public class Lesson_031 {
     public static void main(String[] args) {
         String a = "1234";
-        String b = new String("1234");
+        String b = new String("1234"); //true
         System.out.println(a.matches(b));
+
+        String c = new String("12345"); //false
+        System.out.println(a.matches(c));
+
+        /**
+         * \\d - одна цифра
+         *
+         * */
+        String str1 = "1";
+        String str2 = new String("\\d"); //false
+        System.out.println("str1=" + str1 + " matches(str2=" + str2 +") == " + str1.matches(str2));
     }
 }
